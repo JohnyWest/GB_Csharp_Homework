cbe5fc6a95ef6274df24522b8c0dfbe99b0a20bf
JohnyWest <bbassed@gmail.com>
2022-08-07 04:16:15 +0500

    Добавил метод для задания пользователем размера массива

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
-﻿static void Main()
+﻿namespace Itog // Объявляем пространство имен
 {
-    int number
-    Console.Write()
+    class Programm // Объявляем класс пространства имен
+    {
+        static void Main(string[] args) // Метод Main класса Programm
+        {
+            Console.Write("Введите количество элементов массива: \t");
+            int elementsCount = int.Parse(Console.ReadLine()); // Вводим elementsCount и присваиваем ей ввод пользователя
+            int[] uArray = new int[elementsCount]; //создаём массив на основе elementsCount
+
+            Console.WriteLine("Введенный Вами массив: ");
+            
+            for (int i = 0; i < uArray.Length; i++) //Бегаем по массиву и даём его обратно
+            {
+                Console.WriteLine(uArray[i]);
+            }
+
+
+
+
+
+
+        }
+    }   
 }
