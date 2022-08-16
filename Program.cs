8f4aa66f27a39eb9250e9ff93f3936b000dc7cfe
JohnyWest <bbassed@gmail.com>
2022-08-16 15:43:20 +0500

    Поменял метод создания массива №1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿// 1. метод создания массива строк
-// 2. метод создания нового массива с определенной длиной (например, как в условии <=3 символа)
-// 3. вывод первого и второго массива на консоль
-
-string[] FirstArray()//Метод создания массива 
+﻿string[] FirstArray()//Метод создания массива 
 {
     
     int index = 0;
@@ -15,7 +11,7 @@ string[] FirstArray()//Метод создания массива
         if (text == null) text = String.Empty;// проверка пустых значений
         arrayList.Add(text);
         index++;
-        Console.WriteLine("\tДля продолжения ввода элементов нажмите любую кнопку, для выхода нажмите ESC");
+        Console.WriteLine("\t Для продолжения ввода элементов нажмите любую кнопку, для выхода нажмите ESC");
     }
     while (Console.ReadKey().Key != ConsoleKey.Escape);
 
@@ -26,46 +22,5 @@ string[] FirstArray()//Метод создания массива
     return array;
 }
 
-(int, string[]) SecondArray(string[] array)//Метод создания нового массива
-{
-    int count = 0;
-        for (int i = 0; i < array.Length; i++) if (array[i].Length <= 3) count++;
-            
-    string[] arrayResul = new string[count];
-    return (count, arrayResul);
-}
-
-void FillArray(string[] array, string[] fillArray)//Метод заполнения массива 
-{
-    int index = 0;
-        for (int i = 0; i < array.Length; i++) if (array[i].Length <= 3)
-            {
-                fillArray[index] = array[i];
-                index++;
-           }
-            
-}
-
-string PrintArray(string[] array) // Метод печати массива
-{
-    string result = String.Empty;
-    result += "[";
-    for (int i = 0; i < array.Length; i++)
-    {
-        result += '"' + array[i] + '"';
-        if (i < array.Length - 1) result += ',';
-    }
-    result += "]";
-    return result;
-}
-
-
 Console.WriteLine("Введите с клавиатуры эллементы массива ");
 string[] array = FirstArray(); // Создаю изначальный массив
-(int count, string[] secondArray) = SecondArray(array); // Создаю второй массив
-FillArray(array, secondArray); // Заполняю второй массив в соответствии с условием (вывод тех элементов, чья длина <=3 символов)
-Console.WriteLine();
-Console.WriteLine("Первоначальный массив:");
-Console.WriteLine($"\n{PrintArray(array)} "); //Вывод на консоль изначального массива
-Console.WriteLine("Результат задачи (массив из элементов, длина которых <=3):");
-Console.WriteLine($"\n{PrintArray(secondArray)}"); //Вывод на консоль результата (массив длинной <=3 символа)
