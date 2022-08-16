6b77e404a6b591d4d11693a2d5241a3f30be661f
JohnyWest <bbassed@gmail.com>
2022-08-16 15:32:47 +0500

    Обновил README, Добавил задачу и оформление

--- Program.cs
+++ Program.cs
@@ -1,34 +1,71 @@
-﻿namespace Itog // Объявляем пространство имен
+﻿// 1. метод создания массива строк
+// 2. метод создания нового массива с определенной длиной (например, как в условии <=3 символа)
+// 3. вывод первого и второго массива на консоль
+
+string[] FirstArray()//Метод создания массива 
 {
-    class Programm // Объявляем класс пространства имен
+    
+    int index = 0;
+    string text = String.Empty;
+    List<string> arrayList = new List<string>();
+    do
     {
-        static void Main(string[] args) // Метод Main класса Programm
-        {
-            Console.Write("Введите количество элементов массива: \t");
-            int elementsCount = int.Parse(Console.ReadLine()); // Вводим elementsCount и присваиваем ей ввод пользователя
-            int[] uArray = new int[elementsCount]; //создаём массив на основе elementsCount
-            
-            for (int i = 0; i < uArray.Length; i++) //Бегаем по массиву и даём его обратно
-            {
-                Console.Write($"Введите элемент массива под индексом {i}: \t");
-                uArray[i] = int.Parse(Console.ReadLine()); // Заполнить каждый элемент массива данными пользователя
-            }
-            
-            
-            int[] triArray = new int[]
-            for (int i = 0; i < length; i++)
-            {
-                uArray[i].Length = 
-            }
-            
+        Console.Write($"Элемент массива № {index} : ");
+        text = Console.ReadLine();
+        if (text == null) text = String.Empty;// проверка пустых значений
+        arrayList.Add(text);
+        index++;
+        Console.WriteLine("\tДля продолжения ввода элементов нажмите любую кнопку, для выхода нажмите ESC");
+    }
+    while (Console.ReadKey().Key != ConsoleKey.Escape);
 
-            do
-            {
-                
-            } while (true);
+    string[] array = new string[arrayList.Count];
 
+    for (int i = 0; i < array.Length; i++) array[i] = arrayList[i];
 
-        }
-    }   
+    return array;
 }
-// Не забудь смержить ветку userArray в мастер!!!!!!!!!!!!!
+
+(int, string[]) SecondArray(string[] array)//Метод создания нового массива
+{
+    int count = 0;
+        for (int i = 0; i < array.Length; i++) if (array[i].Length <= 3) count++;
+            
+    string[] arrayResul = new string[count];
+    return (count, arrayResul);
+}
+
+void FillArray(string[] array, string[] fillArray)//Метод заполнения массива 
+{
+    int index = 0;
+        for (int i = 0; i < array.Length; i++) if (array[i].Length <= 3)
+            {
+                fillArray[index] = array[i];
+                index++;
+           }
+            
+}
+
+string PrintArray(string[] array) // Метод печати массива
+{
+    string result = String.Empty;
+    result += "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += '"' + array[i] + '"';
+        if (i < array.Length - 1) result += ',';
+    }
+    result += "]";
+    return result;
+}
+
+
+Console.WriteLine("Введите с клавиатуры эллементы массива ");
+string[] array = FirstArray(); // Создаю изначальный массив
+(int count, string[] secondArray) = SecondArray(array); // Создаю второй массив
+FillArray(array, secondArray); // Заполняю второй массив в соответствии с условием (вывод тех элементов, чья длина <=3 символов)
+Console.WriteLine();
+Console.WriteLine("Первоначальный массив:");
+Console.WriteLine($"\n{PrintArray(array)} "); //Вывод на консоль изначального массива
+Console.WriteLine("Результат задачи (массив из элементов, длина которых <=3):");
+Console.WriteLine($"\n{PrintArray(secondArray)}"); //Вывод на консоль результата (массив длинной <=3 символа)
